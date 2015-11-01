ed53f059b626634f3fdc24ffa25f5f07721a2fb2
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2015-11-01 21:21:22 +0000

    Hooked up search bar to controller

--- app/controllers/courts_controller.rb
+++ app/controllers/courts_controller.rb
@@ -2,5 +2,24 @@ class CourtsController < ApplicationController
 
   def index
     @courts = Court.all
+    @address = session[:address]
+
+    if @address.present?
+      @search_lat = geocode(@address)[0]
+      @search_long = geocode(@address)[1]
+      @closest_name = Court.near(@address).first.name
+      @closest_address = Court.near(@address).first.address
+      @clostest_phone = Court.near(@address).first.phone
+    end
   end
+
+  def get_address
+    session[:address] = params[:address]
+    redirect_to root_path
+  end
+
+  def geocode(address)
+    geocode = Geocoder.coordinates(address)
+  end
+
 end
